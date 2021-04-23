f02279125a021b6dad71b67df0d896240bee6a15
Aleksandra <aleksandra@getrodeo.io>
2021-04-23 11:23:35 +0200

    checking http responses options

--- ilovecats/src/cats/cats.controller.ts
+++ ilovecats/src/cats/cats.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get('names')
-  findAll() {
-    return 'This action returns all the cats'
+  findAll(@Res() response) {
+    response.status(200).send('This action returns all the cats')
   }
 
   @Get(':id')
   findOne(@Param('id') id:string) {
     return `This action returns #${id} cat`
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body
   }
 }
