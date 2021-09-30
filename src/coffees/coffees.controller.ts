7d8b83ee6495576ff89fbea5eaf0a3fea80266fe
Aleksandra <aleksandra@getrodeo.io>
2021-09-30 18:25:24 +0200

    basic service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,30 +1,34 @@
 import { Controller, Get, Post,  Param, Body, HttpCode, HttpStatus, Patch, Delete, Query } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor (private readonly coffeesService: CoffeesService) {}
+
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery
-    return `hello there ${limit} ${offset}`
+    // const { limit, offset } = paginationQuery
+    return this.coffeesService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `return one ${id}`
+    return this.coffeesService.findOne(id)
   }
 
   @Post()
   create(@Body() body) {
-    return body
+    return this.coffeesService.create(body)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return body
+    return this.coffeesService.update(id, body)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return id
+    return this.coffeesService.remove(id)
   }
 }
