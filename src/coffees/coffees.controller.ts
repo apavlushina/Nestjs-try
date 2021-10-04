cd75c53f9e0210e4ce5e80bec6e82d50463d8518
Aleksandra <aleksandra@getrodeo.io>
2021-10-04 11:16:49 +0200

    add validation rules

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post,  Param, Body, HttpCode, HttpStatus, Patch, Delete, Query } from '@nestjs/common';
-import { CoffeesService } from './coffees.service';
-import { Coffee } from './entities/coffee.entity';
+import { Controller, Get, Post,  Param, Body, HttpCode, HttpStatus, Patch, Delete, Query } from '@nestjs/common'
+import { CoffeesService } from './coffees.service'
+import { Coffee } from './entities/coffee.entity'
+import { CreateCoffeeDto } from './dto/create-coffee.dto'
+import { UpdateCoffeeDto } from './dto/update-coffee.dto'
 
 @Controller('coffees')
 export class CoffeesController {
