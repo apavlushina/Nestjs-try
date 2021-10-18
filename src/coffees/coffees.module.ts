0e54ed9f0aca393249302cd9da65d461ebcb8fdc
Aleksandra <aleksandra@getrodeo.io>
2021-10-18 17:19:24 +0200

    async provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,14 +6,23 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto'
 import { Coffee } from './entities/coffee.entity'
 import { Flavor } from './entities/flavor.entity'
 import { COFFEE_BRANDS } from './coffees.constants'
+import { Connection } from 'typeorm'
 
 @Module({ 
   imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController], 
   providers: [CoffeesService, {
-  provide: COFFEE_BRANDS,
-  useValue: ['buddy brew', 'nescafe']
-},
+    provide: 'COFFEE_BRANDS',
+    // Note "async" here, and Promise/Async event inside the Factory function 
+    // Could be a database connection / API call / etc
+    // In our case we're just "mocking" this type of event with a Promise
+    useFactory: async (connection: Connection): Promise<string[]> => {
+      // const coffeeBrands = await connection.query('SELECT * ...');
+      const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe'])
+      return coffeeBrands;
+    },
+    inject: [Connection],
+  },
 ],
   exports: [CoffeesService]
 })
